22b48bb3297c33f534d417687ecab722197175cf
Bc. Róbert Baláž <robert.balaz@student.tuke.sk>
2017-12-02 21:04:04 +0100

    Added demo scene, DICOM image sequence.

---
+++ Leap Motion Tracking/Assets/ImageBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageBehaviour : MonoBehaviour {
+    private float lastChange;
+    string currentImagePath;
+    int currentImageNumber;
+
+    void Start()
+    {
+        lastChange = Time.time;
+        currentImagePath = "Assets/DICOM/image-00001";
+        currentImageNumber = 0;
+    }
+
+    void Update () {
+        if (Time.time - lastChange > 1)
+        {
+            currentImageNumber++;
+            string numberString = currentImageNumber.ToString().PadLeft(6, '0');
+            string path = "Assets/Resources/image-" + numberString + ".jpg";
+            Texture2D texture = (Texture2D)AssetDatabase.LoadAssetAtPath(path, typeof(Texture2D));
+            RawImage image = gameObject.GetComponent<RawImage>();
+            image.texture = texture;
+            lastChange = Time.time;
+            if (currentImageNumber == 361)
+                currentImageNumber = 0;
+        }
+    }
+
+}
+
+
