38dcbeb6e926e6e9f9d8fec929effa0879d98846
jancabadaj <32865801+jancabadaj@users.noreply.github.com>
2017-12-03 13:52:39 +0100

    UI v0.1 - panel, selection buttons

---
+++ Leap Motion Tracking/Assets/Scripts/MenuPanelControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MenuPanelControl : MonoBehaviour {
+
+    public GameObject menuCategoryText;
+    public GameObject serverInfoDisplay;
+    public GameObject sceneSettingsDisplay;
+
+    // Use this for initialization
+    void Start()
+    {
+        menuCategoryText.GetComponent<TextMesh>().text = "Please select category";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void SelectServerInfoMenu()
+    {
+        menuCategoryText.GetComponent<TextMesh>().text = "Server Info";
+    }
+
+    public void SelectSceneSettingsMenu()
+    {
+        menuCategoryText.GetComponent<TextMesh>().text = "Scene Settings";
+    }
+}
